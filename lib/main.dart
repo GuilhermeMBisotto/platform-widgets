4620dc3564f95f9e01868db959423e54c95dcd78
Luca Schifino <luca.schifino@4all.com>
2020-01-15 13:20:14 -0300

    Creating PlatformApp.

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,15 @@ import 'package:android_test_layout/src/ui/HomeTabController.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import './src/extensions/color_extensions.dart';
+import 'src/platform/base/PlatformApp.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-          primaryColor: "#1D1D27".toColor,
-          accentColor: "#34C444".toColor),
+    return PlatformApp(
+      title: 'Platform Widgets',
       home: MyHomePage(),
     );
   }
--- lib/src/platform/base/PlatformApp.dart
+++ lib/src/platform/base/PlatformApp.dart
@@ -2,22 +2,131 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
 
-abstract class PlatformApp extends StatefulWidget {
+class PlatformApp extends StatefulWidget {
+
+  PlatformApp({
+    this.navigatorKey,
+    this.home,
+    this.routes,
+    this.initialRoute,
+    this.onGenerateRoute,
+    this.onUnknownRoute,
+    this.navigatorObservers,
+    this.builder,
+    this.title,
+    this.onGenerateTitle,
+    this.color,
+    this.locale,
+    this.localizationsDelegates,
+    this.localeListResolutionCallback,
+    this.localeResolutionCallback,
+    this.supportedLocales,
+    this.showPerformanceOverlay,
+    this.checkerboardRasterCacheImages,
+    this.checkerboardOffscreenLayers,
+    this.showSemanticsDebugger,
+    this.debugShowCheckedModeBanner,
+    this.shortcuts,
+    this.actions
+  });
+
+  final GlobalKey<NavigatorState> navigatorKey;
+  final Widget home;
+  final Map<String, WidgetBuilder> routes;
+  final String initialRoute;
+  final RouteFactory onGenerateRoute;
+  final RouteFactory onUnknownRoute;
+  final List<NavigatorObserver> navigatorObservers;
+  final TransitionBuilder builder;
+  final String title;
+  final GenerateAppTitle onGenerateTitle;
+  final Color color;
+  final Locale locale;
+  final Iterable<LocalizationsDelegate<dynamic>> localizationsDelegates;
+  final LocaleListResolutionCallback localeListResolutionCallback;
+  final LocaleResolutionCallback localeResolutionCallback;
+  final Iterable<Locale> supportedLocales;
+  final bool showPerformanceOverlay;
+  final bool checkerboardRasterCacheImages;
+  final bool checkerboardOffscreenLayers;
+  final bool showSemanticsDebugger;
+  final bool debugShowCheckedModeBanner;
+  final Map<LogicalKeySet, Intent> shortcuts;
+  final Map<LocalKey, ActionFactory> actions;
+  
+
+  CupertinoApp createIosApp(BuildContext context) { 
+    return CupertinoApp(
+      navigatorKey: navigatorKey,
+      home: home,
+      routes: routes,
+      initialRoute: initialRoute,
+      onGenerateRoute: onGenerateRoute,
+      onUnknownRoute: onUnknownRoute,
+      navigatorObservers: navigatorObservers,
+      builder: builder,
+      title: title,
+      onGenerateTitle: onGenerateTitle,
+      color: color,
+      locale: locale,
+      localizationsDelegates: localizationsDelegates,
+      localeListResolutionCallback: localeListResolutionCallback,
+      localeResolutionCallback: localeResolutionCallback,
+      supportedLocales: supportedLocales,
+      showPerformanceOverlay: showPerformanceOverlay,
+      checkerboardRasterCacheImages: checkerboardRasterCacheImages,
+      checkerboardOffscreenLayers: checkerboardOffscreenLayers,
+      showSemanticsDebugger: showSemanticsDebugger,
+      debugShowCheckedModeBanner: debugShowCheckedModeBanner,
+      shortcuts: shortcuts,
+      actions: actions
+    );
+  }
+  MaterialApp createAndroidApp(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+      home: home,
+      routes: routes,
+      initialRoute: initialRoute,
+      onGenerateRoute: onGenerateRoute,
+      onUnknownRoute: onUnknownRoute,
+      navigatorObservers: navigatorObservers,
+      builder: builder,
+      title: title,
+      onGenerateTitle: onGenerateTitle,
+      color: color,
+      locale: locale,
+      localizationsDelegates: localizationsDelegates,
+      localeListResolutionCallback: localeListResolutionCallback,
+      localeResolutionCallback: localeResolutionCallback,
+      supportedLocales: supportedLocales,
+      showPerformanceOverlay: showPerformanceOverlay,
+      checkerboardRasterCacheImages: checkerboardRasterCacheImages,
+      checkerboardOffscreenLayers: checkerboardOffscreenLayers,
+      showSemanticsDebugger: showSemanticsDebugger,
+      debugShowCheckedModeBanner: debugShowCheckedModeBanner,
+      shortcuts: shortcuts,
+      actions: actions
+    );
+  }
+
   @override
-  _PlatformAppState createState() => null;
+  _PlatformAppState createState() => _PlatformAppState(createIosApp: (context) => createIosApp(context), createAndroidApp: (context) => createAndroidApp(context));
 }
 
-abstract class _PlatformAppState<I extends CupertinoApp, A extends MaterialApp>
-    extends State<PlatformApp> {
-  I _createIosApp(BuildContext context);
-  A _createAndroidApp(BuildContext context);
+class _PlatformAppState<I, A> extends State<PlatformApp> {
+
+  _PlatformAppState({@required this.createIosApp, @required this.createAndroidApp});
+
+  Function createIosApp;
+  Function createAndroidApp;
 
   @override
   Widget build(BuildContext context) {
     if (Platform.isIOS) {
-      return _createIosApp(context);
+      return this.createIosApp(context);
     } else if (Platform.isAndroid) {
-      return _createAndroidApp(context);
+      return this.createAndroidApp(context);
     }
     return Container();
   }
